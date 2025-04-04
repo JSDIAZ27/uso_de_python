e7507f8f16d9ee88ae8915372e0dd4527970065c
jsdiaz27 <juan_s.diaz_v@uao.edu.co>
2025-04-03 20:08:25 -0500

    ejercicio con elif- else  if

--- ejercicios/#ejericico else if.py
+++ ejercicios/#ejericico else if.py
@@ -1,2 +1,20 @@
 #ejericico 1
+"solictiar una edad y clasificarla"
+"en menores de esdad (0 - 17), adultos (18 - 64) y"
+"adultos mayores de (65+)"
+"para este ejercicio usar if elif else "
+"y tambien usar match/case"
+""
 
+#solucion 
+
+edad=int(input("ingrese su edad:"))
+if 0<= edad <= 17:
+    print("eres menor de edad")
+elif 18<= edad <= 64:
+    print("eres un adulto")
+elif edad >= 65:
+    print("eres un adulto mayor")
+else:
+    print("la edad no es valida")
+    
